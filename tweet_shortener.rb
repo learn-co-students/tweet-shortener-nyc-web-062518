dd1944a564d49eadb4fb67ab4e87e7fb32e975af
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-06-18 00:10:33 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,7 +44,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if selective_tweet_shortener(tweet).length > 140
-    selective_tweet_shortener(tweet)[0..139]
+    "#{selective_tweet_shortener(tweet)[0..136]}..."
   else
     selective_tweet_shortener(tweet)
   end
