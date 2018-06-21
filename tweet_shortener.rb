287876b0260a2e688ae055cfd883d55a052ec90c
Daniel Ran <danielr1735@gmail.com>
2018-06-21 15:25:06 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,16 +14,18 @@ def dictionary
 end
 
 def word_substituter(string)
+
   splitTweet = string.split(" ")
-  keys = dictionary.keys
+
   splitTweet.each_with_index { | word, i |
     wordClean = word.downcase.gsub(/[\W\s\d]/, '')
-    if keys.include? wordClean.to_sym
+    if dictionary.keys.include? wordClean.to_sym
       splitTweet[i] = splitTweet[i].downcase.gsub wordClean, dictionary[wordClean.to_sym]
     end
   }
   splitTweet.join(" ")
 end
+
 
 def bulk_tweet_shortener(array)
   array.collect { |x| puts word_substituter(x) }
